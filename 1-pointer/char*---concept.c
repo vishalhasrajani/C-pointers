d7c2d4f662bfc0b14779175cb3d3b45b339cabc6
vishalhasrajani <vish9221@gmail.com>
2020-11-18 16:28:42 -0500

    char* concept

---
+++ 1-pointer/char*---concept.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	char* values[3];
+
+	    values[0] = "Hello";
+	    values[1] = "Mew meww";
+	    values[2] = "Miau miau =3";
+
+
+	    for(int i=0; i<3; i++)
+	    {
+	           printf("%s", values[i]);
+	    }
+
+}
